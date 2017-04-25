70c0240069e571be60af9d254a0b03e5bf828c9a
Allen Sanchez <allenjsanchez@gmail.com>
2017-04-25 17:02:49 +0800

    added controllers and user model

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,3 +1,31 @@
 class ExperiencesController < ApplicationController
 
+  def index
+    @experience = Experience.all
+  end
+
+  def show
+    @experience = Experience.find(params[:id])
+  end
+
+  def new
+    @experience = Experience.new
+  end
+
+  def create
+    @experience = Experience.new(experience_params)
+  end
+
+  private
+
+  def experience_params
+    params.require(:experience).permit(:title,
+                                       :city,
+                                       :description,
+                                       :category,
+                                       :avg_rating,
+                                       :ranking,
+                                       :cost)
+  end
+
 end
